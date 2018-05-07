55c07d767faa069de0f2746466087ec45e226fcf
psuresan12 <psuresan12@gmail.com>
2018-05-08 02:36:26 +1000

    update controller to implement pagination

--- Magic_Inventory/Controllers/OwnerController.cs
+++ Magic_Inventory/Controllers/OwnerController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Magic_Inventory.Data;
 using Microsoft.EntityFrameworkCore;
+using Magic_Inventory.Utility;
+using Magic_Inventory.ViewModel;
+using Magic_Inventory.Models;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,8 +25,12 @@ namespace Magic_Inventory.Controllers
 
 
         // Auto-parsed variables coming in from the request - there is a form on the page to send this data.
-        public async Task<IActionResult> Index(string productName)
+        public async Task<IActionResult> Index(
+           string sortOrder, string currentFilter,
+            string productName, int? page)
         {
+            ViewBag.CurrentSort = sortOrder;
+
             // Eager loading the Product table - join between OwnerInventory and the Product table.
             var query = _context.OwnerInventory.Include(x => x.Product).Select(x => x);
 
@@ -36,15 +44,31 @@ namespace Magic_Inventory.Controllers
                 ViewBag.ProductName = productName;
             }
 
+            if (productName != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                productName = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = productName;
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
             // Adding an order by to the query for the Product name.
-           //// query = query.OrderBy(x => x.ProductID);
+            //// query = query.OrderBy(x => x.ProductID);
 
             // Passing a List<OwnerInventory> model object to the View.
-            return View(await query.ToListAsync());
+            return View(await PaginatedList<OwnerInventory>
+                        .CreateAsync(query.AsNoTracking(), page ?? 1, pageSize));
         }
 
 
-        public String Welcome(){
+        public String Welcome()
+        {
             return "this is a owner page";
         }
     }
