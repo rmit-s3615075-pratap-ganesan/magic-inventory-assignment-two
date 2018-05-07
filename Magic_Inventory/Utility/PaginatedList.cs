6a19a11f1abd03df0d676391c0f4fdcc0d39419d
psuresan12 <psuresan12@gmail.com>
2018-05-08 02:24:34 +1000

    basic code to pagination

---
+++ Magic_Inventory/Utility/PaginatedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Magic_Inventory.Utility
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        // The CreateAsync method in this code takes page size and page number and 
+        // applies the appropriate Skip and Take statements to the IQueryable. 
+        // When ToListAsync is called on the IQueryable, it will return a List containing 
+        // only the requested page. The properties HasPreviousPage and HasNextPage can be 
+        // used to enable or disable Previous and Next paging buttons.
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
---
+++ Magic_Inventory/ViewModel/OwnerInventoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Magic_Inventory.ViewModel
+{
+    public class OwnerInventoryViewModel
+    {
+        public int ProductID { get; set; }
+        public int StockLevel { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public OwnerInventoryViewModel(int ProductID,int StockLevel,string Name,decimal price){
+            this.ProductID = ProductID;
+            this.StockLevel = StockLevel;
+            this.Name = Name;
+            this.Price = price;
+        }
+    }
+}
